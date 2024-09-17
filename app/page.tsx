df261c22d0cc472f12a64abc7257cba0b78ce985
zhaoclaire <181871418+zhaoclaire@users.noreply.github.com>
2024-09-17 17:00:16 -0400

    Add first blog

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,39 @@
 "use client";
-import { Heading, Text } from "@chakra-ui/react";
-import Math from "@/components/Math";
+
+import {
+  Center,
+  Container,
+  Heading,
+  Highlight,
+  Link,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+
+import NextLink from "next/link";
+
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <>
-      <Heading>{' { Blog }'}</Heading>
-      <Text fontSize={'3xl'}>ABC abc</Text>
-      Hello world <Math latex='\int xyz \sum' display={ false }></Math>
+      <Center className={styles.title}>
+        <Heading>Blogs</Heading>
+      </Center>
+      <VStack>
+        <Container maxW="container.lg" fontSize="xl">
+          {/* <Highlight
+            query={[""]}
+            styles={{ px: "2", py: "1", rounded: "full", bg: "teal.100" }}
+          >
+            
+          </Highlight> */}
+          1.{" "}
+          <Link as={NextLink} href="/video-poet">
+            Reading of the VideoPoet paper on LLM video generation.
+          </Link>
+        </Container>
+      </VStack>
     </>
   );
 }
